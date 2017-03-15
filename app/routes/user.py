7642472438f221b8f2f2dc705ba7defa2fff0862
Leon Zaruvinsky <leonzaruvinsky@gmail.com>
2017-03-14 21:17:46 -0400

    routes

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from app import db, app
-from models import User, Post
+from models import User
 
 
 # ROUTES FOR USER
@@ -86,59 +86,3 @@ def get_all_users_posts(email):
         })
 
 
-# ROUTES FOR POSTS
-# Get a json of all posts in the db
-@app.route('/api/posts', methods=['GET'])
-def get_all_posts():
-    return jsonify(posts=[i.serialize for i in Post.query.all()])
-
-
-# Create a new post from the user
-# Note: Do this by sending in {
-#                               "post" : "<Hello, I had a question...>"
-#                               } as a json
-@app.route('/api/users/<string:email>/posts', methods=['POST'])
-def create_post(email):
-    user = User.query.filter(User.email==email).first()
-    r = request.get_json(force=True) 
-    if user:
-        post = Post(
-            user_id=user.id,
-            post=r.get("post", None)
-        )
-        user.create_post(post)
-        db.session.add(user)
-        db.session.add(post)
-        db.session.commit()
-        return jsonify(messages=user.serialize_posts)
-    else:
-        return jsonify({
-            "Error": "User Not Found"
-            })
-
-
-# Delete a post for the user
-# Note: Do this by sending in {"post_id" : "<Post Id>" as a json
-@app.route('/api/users/<string:email>/posts', methods=['DELETE'])
-def delete_post(email):
-    user = User.query.filter(User.email==email).first()
-    r = request.get_json(force=True)
-    if r.get('post_id'):
-        post = Post.query.filter(Post.id==r.get('post_id')).first() 
-    if user:
-        if post:
-            if user.has_post(post):
-                user.delete_post(post)
-                db.session.add(user)
-                db.session.commit()
-                return jsonify(updated_posts=user.serialize_posts)
-            else:
-                return jsonify({"Error": "Post does not exist!"})
-        else:
-            return jsonify({
-                "Error": "Post Not Found"
-                })
-    else:
-        return jsonify({
-            "Error": "User Not Found"
-            })
