c9701894d9e823914771b76e63729714ae6b1746
Leon Zaruvinsky <leonzaruvinsky@gmail.com>
2017-03-16 20:44:27 -0400

    profs

--- server/models/post.py
+++ server/models/post.py
@@ -2,12 +2,12 @@ from server import db
 
 
 class Post(db.Model):
-    __tablename__ = 'post'
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(120))
     desc = db.Column(db.String(10000))
     qualifications = db.Column(db.String(10000))
-    professor_id = db.Column(db.String(64), db.ForeignKey('professor.net_id'))
+    professor_id = db.Column(db.String(64), db.ForeignKey('professors.net_id'))
     current_students = db.Column(db.String(10000))
     desired_skills = db.Column(db.String(10000))
     capacity = db.Column(db.Integer)
--- server/models/professor.py
+++ server/models/professor.py
@@ -2,31 +2,12 @@ from server import db
 
 
 class Professor(db.Model):
-    __tablename__ = 'professor'
+    __tablename__ = 'professors'
     net_id = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='professor', lazy='dynamic')
     desc = db.Column(db.String(10000))
     interests = db.Column(db.String(10000))
-#
-#    # Post Helper Funcs
-#    # Posts had unique creator and cannot be added twice to a user
-#    def create_post(self, post):
-#        if post.user_id is None:
-#            if not self.has_post(post):
-#                self.posts.append(post)
-#                return self
-#        else:
-#            print("This post is already affiliated with a user")
-#
-#    def delete_post(self, post):
-#        if self.has_post(post):
-#            self.posts.remove(post)
-#            return self
-#
-#    def has_post(self, post):
-#        return self.posts.filter(Post.id == post.id).count() > 0
 
     # This is to convert calls for User into json friendly format!
     @property
@@ -39,11 +20,3 @@ class Professor(db.Model):
             'desc': self.desc,
             'interests': self.interests
         }
-
-#    @property
-#    def serialize_posts(self):
-#        return [item.serialize for item in self.posts]
-#
-    def __repr__(self):
-        return '<Professor %r>' % (self.name)
-
