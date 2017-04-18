90a9adc904ad41f44d726702c9e94e3fd6bb1763
imikewhite <michaelhwhite13@gmail.com>
2017-04-18 12:19:22 -0400

    as

--- server/views.py
+++ server/views.py
@@ -25,7 +25,7 @@ def index(tags=None, all=None, posts=None):
         post['professor_name'] = Professor.get_professor_by_netid(
             post['professor_id']).name
 
-    print posts
+    posts.sort(key=lambda x: x['date_created'], reverse=True)
     return render_template(
         "index.html",
         title='Home',
