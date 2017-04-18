3e9bb32b3080f5623f4c62281a6f405f2ccd7424
Leon Zaruvinsky <leonzaruvinsky@gmail.com>
2017-04-18 02:15:49 -0400

    fixed get post

--- server/models/post.py
+++ server/models/post.py
@@ -128,6 +128,9 @@ class Post(db.Model):
 
     @classmethod
     def get_post_by_id(cls, post_id):
+        if not post_id:
+            return None
+
         post = Post.query.filter(Post.id == int(post_id)).first()
         if post:
             return post
