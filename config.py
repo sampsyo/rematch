a2c790845318f4fc8f1a0d8930a0361ffbea5df6
Leon Zaruvinsky <leonzaruvinsky@gmail.com>
2017-05-05 01:43:38 -0400

    base pagination

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Pr0f3ss0r-ArmS-Is-Br1T1sH'
 
+BASE_URL = 'http://localhost:5000'
+PAGINATION_PER_PAGE = 2
+
 
 # TODO: For Searches through the DB, for example if you wanted to search for a specific professor. 
 # Not implemented but put here for ease of use in the future:
