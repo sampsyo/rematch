110cca7e60fb54a314d7e6c39e7aef39885c5932
Leon Zaruvinsky <leonzaruvinsky@gmail.com>
2017-05-18 17:37:30 -0400

    fixed

--- server/views.py
+++ server/views.py
@@ -87,9 +87,10 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
-@app.route('/profile/<net_id>', methods=['GET', 'POST'])
+@app.route('/profile/<net_id>', methods=['GET'])
 @login_required
 def profile(net_id):
+    import ipdb; ipdb.set_trace()
     if not current_user.net_id == net_id:
         return redirect("/", code=301)
 
